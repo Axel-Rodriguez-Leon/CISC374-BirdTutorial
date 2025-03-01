6c71462a4a3525e22d7dbe4f2ff0989830c84e0a
Axel Rodriguez-Leon <axelarl@udel.edu>
2025-03-01 00:07:24 -0500

    Added difficulty
    -Every time the player scores 10 points, the speed of the pipes increase by 10

--- Assets/Scripts/PipesController.cs
+++ Assets/Scripts/PipesController.cs
@@ -5,6 +5,7 @@ public class PipesController : MonoBehaviour
     public float moveSpeed = 25;
     public float deadZone = -45;
     public LogicScript logic;
+    private float difficultyLevel = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,10 +17,20 @@ public class PipesController : MonoBehaviour
     {
         transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
 
+
         if (transform.position.x < deadZone)
         {
             Debug.Log("Pipe Deleted");
             Destroy(gameObject);
+        }
+    }
+
+    public void increaseDifficulty()
+    {
+        if (logic.playerScore > difficultyLevel)
+        {
+            moveSpeed += 10;
+            difficultyLevel += 10;
         }
     }
 }
